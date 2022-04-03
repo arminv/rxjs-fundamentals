25b5aabcbbd8b70492912ec010a7959f1e1e7e0a
arminv <arminv@users.noreply.github.com>
2022-04-03 04:32:34 -0400

    End of Section 'Intervals & Timers'

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -3,3 +3,26 @@ import { setCount, startButton, pauseButton } from './utilities';
 
 const start$ = fromEvent(startButton, 'click');
 const pause$ = fromEvent(pauseButton, 'click');
+
+let interval$;
+
+start$.subscribe(() => {
+  interval$ = interval(1000).subscribe(setCount);
+});
+
+pause$.subscribe(() => {
+  interval$.unsubscribe();
+});
+
+// We can also store the subscription:
+
+let interval$ = interval(1000);
+let subscription;
+
+start$.subscribe(() => {
+  subscription = interval$.subscribe(setCount);
+});
+
+pause$.subscribe(() => {
+  subscription.unsubscribe();
+});
